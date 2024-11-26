693c2f93f8a1f25ff9ea7e498457190c6a08bc5f
SystemSolution21 <156997764+SystemSolution21@users.noreply.github.com>
2024-11-26 17:19:22 +0900

    Initial commit

---
+++ tennis_club/members/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(route="", view=views.index, name="index"),
+    path(route="members/", view=views.members, name="members"),
+    path(route="members/details/<int:id>", view=views.details, name="details"),
+]
---
+++ tennis_club/members/views.py
@@ -0,0 +1,28 @@
+from django import template
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+
+def index(request) -> HttpResponse:
+    template = loader.get_template(template_name="index.html")
+    return HttpResponse(content=template.render())
+
+
+def members(request) -> HttpResponse:
+    members = Member.objects.all().values()
+    template = loader.get_template(template_name="members.html")
+    context = {
+        "members": members,
+    }
+    return HttpResponse(content=template.render(context=context, request=request))
+
+
+def details(request, id) -> HttpResponse:
+    members = Member.objects.get(id=id)
+    template = loader.get_template(template_name="details.html")
+    context = {
+        "members": members,
+    }
+    return HttpResponse(content=template.render(context=context, request=request))
