41cf2003e281d4d49fe3b5c1d100b08c7cca793b
SystemSolution21 <156997764+SystemSolution21@users.noreply.github.com>
2024-11-27 22:59:56 +0900

    Create globalstaticfiles

--- tennis_club/tennis_club/settings.py
+++ tennis_club/tennis_club/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "tennis_club.urls"
@@ -116,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [BASE_DIR / "globalstaticfiles"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
