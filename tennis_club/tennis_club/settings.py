9dd41b5150b0ed13c055dc610c09ccd977e74006
SystemSolution21 <systemsolution21@gmail.com>
2025-12-18 17:25:00 +0900

    modified: settings.py, new file: base_bootstrap5.html

--- tennis_club/tennis_club/settings.py
+++ tennis_club/tennis_club/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "members.apps.MembersConfig",
+    # "django_bootstrap5", # Bootstrap5
 ]
 
 MIDDLEWARE = [
