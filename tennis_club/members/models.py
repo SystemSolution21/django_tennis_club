e10eb2e6f91ce4ccd021a9f375efd03100599fae
SystemSolution21 <systemsolution21@gmail.com>
2025-12-18 12:59:06 +0900

    modified: .gitignore, tennis_club/members/migrations/0003_member_slug.py, tennis_club/members/models.py

--- tennis_club/members/models.py
+++ tennis_club/members/models.py
@@ -6,6 +6,7 @@ class Member(models.Model):
     lastname = models.CharField(max_length=255)
     phone = models.IntegerField(null=True)
     join_date = models.DateField(null=True)
+    slug = models.SlugField(null=False, default="")
 
     def __str__(self) -> str:
         return f"{self.firstname} {self.lastname}"
